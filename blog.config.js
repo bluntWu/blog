df678be44ff3e9d7bc337a2ce6c8625440a87d77
bluntWu <1109672744@qq.com>
2023-09-18 18:27:29 +0800

    second commit

--- blog.config.js
+++ blog.config.js
@@ -1,11 +1,11 @@
 const BLOG = {
-  title: 'Notionic',
-  author: '左蓝',
-  email: 'i@zuolan.me',
+  title: "Cuicuishayu",
+  author: 'W K',
+  email: 'wukaiczy@gmail.com',
   link: 'https://zuolan.me',
   newsletter: 'Notionic Weekly',
-  description: 'A static blog build on top of Notion and Next.js',
-  lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
+  description: 'O ever youthful, O ever weeping',
+  lang: 'zh-CN', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
